4800cb7dcc51ad5e3974a785380d59649d511cae
manojkumar.r <manojkumar.r@bikayi.com>
2023-01-25 13:29:08 +0530

    Injected bik script

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import {Route , BrowserRouter, Switch} from 'react-router-dom';
-import React from 'react';
+import React, {useEffect} from 'react';
 import Navbar from './components/navbar'
 import Home from './components/home';
 import Search from './components/search';
@@ -7,6 +7,13 @@ import Details from './components/details';
 import Pagenotfound from './components/404';
 
 const Router = () => {
+
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = "<script type=\"text/javascript\" id=\"bikScript\" async src=\"https://api.staging.bik.ai/dm/storeFrontScriptsApiFunctions-serveBikScripts/?store=https://63c951acdc72e600093c8ec7--genuine-mousse-6c4307.netlify.app/\"></script>";
+        script.async = true;
+        document.head.appendChild(script);
+    }, []);
 
     return(
         <BrowserRouter>
